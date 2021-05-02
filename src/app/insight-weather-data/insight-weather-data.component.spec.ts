793481957e7cab9ec0a5a55d41837837d68f9163
Aditya Inapurapu <adityaii@gmail.com>
2021-05-02 13:51:09 -0700

    Fixed insight-weather-data component unit test

--- src/app/insight-weather-data/insight-weather-data.component.spec.ts
+++ src/app/insight-weather-data/insight-weather-data.component.spec.ts
@@ -8,9 +8,9 @@ describe('InsightWeatherDataComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InsightWeatherDataComponent ]
+      declarations: [InsightWeatherDataComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,11 @@ describe('InsightWeatherDataComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Insight Weather Data');
   });
 });
