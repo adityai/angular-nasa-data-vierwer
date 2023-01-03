92bde76fe73d681f472c730d9f98b15027efaf63
adityaii <adityaii@gmail.com>
2023-01-03 17:47:35 -0500

    Adding perseverance service wip

--- src/app/perseverance-photos.service.ts
+++ src/app/perseverance-photos.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerseverancePhotosService {
 
-  constructor() { }
+  public url = "https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=5M12ifePfRKP7c9ywgRFXLYq5J8JHasG8zOKaect"
+  constructor(private http: HttpClient) { }
+
+  getPerseverancePhotoLatest(): Observable<any> {
+    return this.http.get<any>(this.url);
+  }
 }
--- src/app/perseverance-photos/perseverance-photos.component.ts
+++ src/app/perseverance-photos/perseverance-photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PerseverancePhotosService } from '../perseverance-photos.service';
 
 @Component({
   selector: 'app-perseverance-photos',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perseverance-photos.component.css']
 })
 export class PerseverancePhotosComponent implements OnInit {
+  public data: any;
+  public imageUrl: any;
+  public earthDate: any;
 
-  constructor() { }
+  constructor(private service: PerseverancePhotosService) { }
 
   ngOnInit(): void {
+    this.service.getPerseverancePhotoLatest().subscribe(data => {
+      this.data = data;
+      let jsonObject = JSON.parse(JSON.stringify(this.data));
+      this.imageUrl = jsonObject.photos[0].img_src;
+      this.earthDate = jsonObject.photos[0].earth_date;
+      console.log(this.earthDate);
+    });
+
   }
 
 }
